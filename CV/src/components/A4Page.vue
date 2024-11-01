a35658fa51fb3d48f2a155b6341a22ea2a637bb5
Elora-V <elora95.vigo@gmail.com>
2024-11-01 13:16:12 +0100

    page A4

---
+++ CV/src/components/A4Page.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="A4">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.A4 {
+  width: 215mm;
+  height: 307mm;
+  background-color: blue;
+}
+</style>
--- CV/src/views/CV.vue
+++ CV/src/views/CV.vue
@@ -1,1 +1,22 @@
-<template></template>
+<template>
+    <div class="container">
+        <A4Page>
+            dfhkuv
+        </A4Page>
+    </div>
+</template>
+
+<script setup lang="ts">
+import A4Page from '@/components/A4Page.vue';
+</script>
+
+<style scoped>
+
+.container {
+  background-color: red;
+  display: flex;
+  align-items: center; /**vertical alignment**/
+  justify-content: center; /**horizontal alignment**/
+}
+
+</style>
