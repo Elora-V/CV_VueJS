a04e87597795cbed18a65335171708e58e90d68f
Elora-V <elora95.vigo@gmail.com>
2024-11-02 18:00:21 +0100

    photo

---
+++ CV/src/components/Photo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="containerSkills">
+        <div class="circular-image">
+            <img src="@/assets/images/CV-photo.png" alt="Portrait CV">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.containerSkills{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.circular-image {
+    width: 170px;            
+    height: 170px;           
+    border-radius: 50%; 
+    border: solid 2px white;
+    overflow: hidden;  /* hide what is not inside border */
+}
+
+.circular-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;       /* cover circle without distortion */
+}
+
+</style>
--- CV/src/main.ts
+++ CV/src/main.ts
@@ -1,4 +1,4 @@
-import './assets/color.css';
+import './assets/css/color.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
