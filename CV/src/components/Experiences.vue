c11feddadacf30e4daca0729488069ce40c2162b
Elora-V <elora95.vigo@gmail.com>
2024-11-10 20:03:04 +0100

    jobdetails list

--- CV/src/components/Experiences.vue
+++ CV/src/components/Experiences.vue
@@ -1,39 +1,69 @@
 <template>
-    <ItemList 
-    title="title" 
-    location="toulouse" 
-    date="mars 2024-juin 2024"
-    firm="INRAE"
-    >
-    blabllskhkhflqkhlhvsjdlkhfsljhlskdjhvlkjhsklhj\nuhdfoshdohgkus\nsdfhsh\nfftfjf
-    jhljhgkjvkhgv
-    fjf
-    </ItemList>
 
-    <ItemList 
-    title="title" 
-    location="toulouse" 
-    date="mars 2024-juin 2024"
-    firm="INRAE"
+    <div class="title"> Expériences </div>
+    <JobDetail v-for="(experience, index) in experiences" :key="index"
+    :job-details="experience"
     >
-    blabllskhkhflqkhlhvsjdlkhfsljhlskdjhvlkjhsklhj\nuhdfoshdohgkus\nsdfhsh\nfftfjf
-    jhljhgkjvkhgv
-    fjf
-    </ItemList>
+    <div v-html="experience.description"></div>
+    </JobDetail>
 
-    <ItemList 
-    title="title" 
-    location="toulouse" 
-    date="mars 2024-juin 2024"
-    firm="INRAE"
-    >
-    blabllskhkhflqkhlhvsjdlkhfsljhlskdjhvlkjhsklhj\nuhdfoshdohgkus\nsdfhsh\nfftfjf
-    jhljhgkjvkhgv
-    fjf
-    </ItemList>
 </template>
 
 
 <script setup lang="ts">
-import ItemList from "./ItemListXP.vue"
+import type { JobDetails } from "@/models/JobDetails";
+import JobDetail from "./JobDetail.vue"
+
+const experiences:JobDetails[]=[
+    {
+        title:"Ingenieur d'étude",
+        firm:"INRAE",
+        team:"MeX",
+        location:"Toulouse, saint-martin-du-touch",
+        date:"septembre-decembre 2024",
+        skills:["Typescript","Jest","Vue.js","Vuetify"],
+        description:"Rendre disponible un algorithme : tests unitaires,\
+        documentation, packaging et panel utilisateur."
+    },
+    {
+        title:"Stage : Developpement d'un algorithme",
+        firm:"INRAE",
+        team:"MeX",
+        location:"Toulouse, saint-martin-du-touch",
+        date:"mars-aout 2024",
+        skills:["Typescript","Théorie des graphes","Bibliographie"],
+        description:"Developpement d'un algorithme de dessin de graphe \
+        adapté aux réseaux métaboliques."
+    },
+    {
+        title:"Stage : Modification d'interface web",
+        firm:"INRAE",
+        team:"MaIAGE",
+        location:"Jouy-en-Josas",
+        date:"mai-juin 2023",
+        skills:["HTML","CSS","Javascript","Python","Flask","SQL"],
+        description:"Intégration d'une information de fiabilité dans la base \
+        de données et dans l'interface web d'Omnicrobe."
+    },
+    {
+        title:"TER : Reproductibilité des workflows",
+        firm:"LISN",
+        location:"Orsay",
+        date:"octobre-decembre 2022",
+        skills:["git","Python","Docker"],
+        description:"Travaux d'Etude et de Recherche sur l'évaluation de \
+        la reproductibilité des workflows Nextflow présent sur github."
+    },
+    {
+        title:"Stage : Etude comportementale chez les rats",
+        firm:"NeuroPSI",
+        team: "Mémoire, émotion, et temps",
+        location:"Orsay",
+        date:"mai-juin 2021",
+        skills:["R","Bibliographie"],
+        description:"Etude comportementale de l'estimation du temps \
+        chez les rats."
+    }
+
+]
 </script>
---
+++ CV/src/models/JobDetails.ts
@@ -0,0 +1,10 @@
+export interface JobDetails {
+    title: string;           
+    firm?: string;           
+    team?: string;           
+    location?: string;       
+    date?: string;           
+    skills?: string[];       
+    description?: string;    
+  }
+  
